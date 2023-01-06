b1a73ac58ce9f4e9e6b3a35a15e788a77f5c82bb
toshinori8 <zbigniew.karski@gmail.com>
2023-01-06 13:35:10 +0100

    Create Battery status for C3

---
+++ src/battery.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include "esp_adc_cal.h"
+
+
+// Battery setup
+#define BAT_ADC    2
+float Voltage = 0.0;
+uint32_t readADC_Cal(int ADC_Raw);
+
+void batteryInfo(){
+
+
+
+
+}
