dc4514ba7b9a1c2fd28c0cbbf07ba542bdf36eb7
Adam Karski <zbigniew.karski@gmail.com>
2023-01-15 17:52:52 +0100

    select temp fix

--- data/assets/index.js
+++ data/assets/index.js
@@ -1,132 +1,160 @@
 
 
 class Room {
-  constructor(id, name, temperature, minTemp, sensorMac) {
+  constructor(id, name, temperature, humidity, minTemp, sensorMac) {
     this.id           = id;
     this.name         = name;
     this.temperature  = temperature;
     this.minTemp      = minTemp;
+    this.humidity     = humidity;
     this.sensorMac    = sensorMac;
   }
 }
 
 class Termostat {
   constructor() {
+   
     this.rooms    = [];
     this.devices  = [];
+
   }
 
-  addRoom(id, name, temperature, minTemp, sensorMac) {
-    this.rooms.push(new Room(id, name, temperature, minTemp, sensorMac));
-
-
-    $("#roomContainer").append(`<div class="room_element room_element_`+id+` sha_temp_body">
+  addRoom(id, name, temperature, humidity, minTemp, sensorMac) {
+  
+    this.rooms.push(new Room(id, name, temperature, humidity, minTemp, sensorMac));
+    console.log(id);
+    this.createRoom(id);
     
-    <div class="switch">
-          <span class="sha_switch pull-right off swithTheme x2 enable_heat"></span>
-        </div>
-        <div class="encoder">
-          <input class="dial noselect" value="0" min-value="12" max-value="36">
-          <div class="backx"><div class="heat_value">80</div></div>
-        </div>
-        <div class="row top-buffer ">
-            <div class="col-12">
-                <span class="sha_temp white_back">
-                    <span>
-                        <span class="temp-data">1<span class="small_01">.2</span> <sup>°C</sup></span>
-                            <hr class="line_">
-                        <span class="hum-data">0<span class="small_01">.0</span>
-                        <span class="sup">%</span>
-                        <span class="hidden_span mac_device">`+sensorMac+`</span>
-                </span>
-                </span>
-                </span>
-        </div>
-        <div class="menu_room">
-            <h1 class="roomName">`+name+`</h1>
-        </div>
-        <div class="height_auto">
-        </div>
-        </div>
-
-    </div>`);
-
-
-    
+  
   }
 
-  createEncoders(){
-    console.log(this.rooms);
-    this.rooms.forEach((room) => {
-        // show encoder on mouseenter
-        // document.querySelector(".room_element_"+room.id +" .encoder").addEventListener("mouseenter", function(){
-        //   console.log("mouseenter");
-        //   console.log(this);
-        //   $(this).fadeIn(200);
+  createEncoder(id){
+   let  element = document.querySelector(`#room-${id} #encoder-${id}`);  
+    console.log(element);
 
-        // });
+    // element.setAttribute("style", `
+    // position: re;
+    // top: 45px;
+    // opacity:0.4;
+    // left: 35px;
+    // width: 230px;
+    // height: 229px;
+    // z-index: 40000;
+    // `);
+    // console.log(document.querySelector("#room-0"));
+    element.addEventListener("mouseleave", function(){
+
+      $(this).removeClass("visible");
+
+
+    });
+    element.addEventListener("mouseenter", function(){
+
+
+//      console.log(`mouseenter $#room-${id} #encoder-${id}`);
+    //   // console.log($("#room-0 #encoder-0"));
+     // console.log(this);
+    $(this).addClass("visible");
+     let elementUpdate= $(this).find(".heat_value");
+     console.log(elementUpdate);
+        $(this).find(".dial").knob({
+              readOnly: false,
+              height: 220,
+              width: 220,
+              min: 12,
+              max: 36,
+              step: 0.1,
+              thickness: 0.15,
+              displayInput: false,
+              dynamicDraw: true,
+              fgColor: "#7ba8c9",
+              bgColor: "none",
+              change: function (v) {
+                //val = v.toString().slice(0, 4);
+                console.log(v.toString().slice(0, 4));
+                $(elementUpdate).html(v.toString().slice(0, 4));
+                // console.log();
+              }
+        });
 
 
     });
 
-    // // create encoders for every room
-    // let encoders = document.querySelectorAll(".dial");
-    // encoders.forEach((encoder) => {
-    //   $(encoder).knob({
-    //     change: function (value) {
-    //       //console.log("change : " + value);
-    //     },
-    //     release: function (value) {
-    //       //console.log(this.$.attr('value'));
-    //       //console.log("release : " + value);
-    //     },
-    //     cancel: function () {
-    //       //console.log("cancel : ", this);
-    //     },
-    //   });
-    // });
+   
+
 
 
   }
 
   updateTemp(id, newTemperature) {    
+
+    // find room with id
+    let room = this.rooms.find((room) => room.id == id);
+    room.temperature = newTemperature;
+
     let temp = newTemperature.toString().split(".");
-    document.querySelector(".room_element_"+id+" .temp-data").firstChild.data=temp[0];
-    document.querySelector(".room_element_"+id+" .temp-data span").firstChild.data="."+temp[1];  
+    document.querySelector("#room-"+id+" .temp-data").firstChild.data=temp[0];
+    if(temp[1] == undefined){temp[1]="0"};
+    document.querySelector("#room-"+id+" .temp-data span").firstChild.data="."+temp[1];  
+  
   }
   updateHum(id, newHumidity) {
+  
+    let room = this.rooms.find((room) => room.id == id);
+    room.humidity = newHumidity;
+
     let hum = newHumidity.toString().split(".");
-    document.querySelector(".room_element_"+id+" .hum-data").firstChild.data=hum[0];
-    document.querySelector(".room_element_"+id+" .hum-data span").firstChild.data="."+hum[1];  
+    document.querySelector("#room-"+id+" .hum-data").firstChild.data=hum[0];
+    if(hum[1] == undefined){hum[1]="0"};
+    document.querySelector("#room-"+id+" .hum-data span").firstChild.data="."+hum[1];  
+  
   }
 
   
   
-  createRooms() {
+  createRoom(id) {
 
-    let container = document.querySelector("#roomContainer");
     
-    if (this.rooms.length > 0) {
-      
-      // create ID for every room
-      let id = 0;
-
-      this.rooms.forEach((room) => {
-        
+        let room = this.rooms.find((room) => room.id == id);
+        let container = document.querySelector("#roomContainer");
         let newRoom = document.createElement("div");
+        
+        newRoom.id = `room-${room.id}`;
+        newRoom.innerHTML = `
+        <div class="room_element sha_temp_body">
+          
+            <div  id="encoder-${room.id}" class="enc">
+              <input class="dial noselect" value="0" min-value="12" max-value="36">
+              <div class="backx"><div class="heat_value">80</div></div>
+            </div>
+            <div class="row top-buffer ">
+                <div class="col-12">
+                    <span class="sha_temp white_back">
+                        <span>
+                            <span class="temp-data">1<span class="small_01">.2</span> <sup>°C</sup></span>
+                                <hr class="line_">
+                            <span class="hum-data">0<span class="small_01">.0</span>
+                            <span class="sup">%</span>
+                            <span class="hidden_span mac_device">`+room.sensorMac+`</span>
+                    </span>
+                    </span>
+                    </span>
+            </div>
+            <div class="menu_room">
+                <h1 class="roomName">`+room.name+`</h1>
+            </div>
+            <div class="height_auto">
+            </div>
+            </div>
+        </div>`;
+
         container.appendChild(newRoom);
-
-      });
-    } else {
-      let message = document.createElement("div");
-      message.innerHTML = '</br></br><div class="alert alert-secondary"><strong>Brak pomieszczeń</strong></br>Być może wystąpił problem z połączeniem.</div>';
-      container.appendChild(message);
-    }
-   // this.createEncoders()
-    return true;
+        this.createEncoder(room.id);
   }
+  
 
-}
+}// end class Termostat
+
 
 function interaction() {
   $("#sha_button_list li").click(function () {
@@ -182,14 +210,11 @@ document.addEventListener("DOMContentLoaded", function () {
       
       for (let roomData of data) {
         console.log(roomData);
-        termostat.addRoom(roomData.id, roomData.name, roomData.temperature, roomData.minTemp, roomData.sensorMac);
+        termostat.addRoom(roomData.id, roomData.name, roomData.temperature, roomData.minTemp,roomData.sensorMac);
       }
-    //termostat.createRooms();
-    //termostat.updateTemp(2,1.13);
-    //termostat.updateHum(2,45.4);
     })
     .catch((error) => {
-      console.log(error + "error getting Rooms");
+      console.log(error + "error creating Rooms");
     });
 
   // interaction();
@@ -199,23 +224,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
   
-  // $(".room_element_0 .encoder .dial").knob({
-  //   readOnly: false,
-  //   height: 220,
-  //   width: 220,
-  //   min: 12,
-  //   max: 36,
-  //   step: 0.1,
-  //   thickness: 0.15,
-  //   displayInput: false,
-  //   dynamicDraw: true,
-  //   fgColor: "#7ba8c9",
-  //   bgColor: "none",
-  //   change: function (v) {
-  //     val = v.toString().slice(0, 4);
-  //     $(".room_element_1 .encoder .heat_value").html(val);
-  //   }
-  // });
+ 
     
 
 
